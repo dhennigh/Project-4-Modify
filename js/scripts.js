acf9bc7f4fd4c25c679e1bc43673876b06cad2a8
Darin Hennigh <dchennigh@gmail.com>
2019-05-22 20:35:52 -0500

    Playing with accessing DOM and changing/adding to Markup.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,12 @@ const paragraphs = document.getElementsByTagName('p');
 const firstArticle = articles[0];
 const secondParagraph = paragraphs[1];
 const whiteTextElements = document.getElementsByClassName('text-white');
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.querySelector('aside');
+/*const sidebar = document.getElementById('sidebar');
+*/
 
 // Modify the DOM
+/*
 const mainHeading = document.getElementById('main-heading');
 mainHeading.textContent = 'Modify the DOM';
 
@@ -34,3 +37,24 @@ newArticle.classList.add('m-2','p-2','border','border-secondary');
 newArticle.setAttribute('id','art-004');
 const main = document.querySelector('main');
 main.appendChild(newArticle);
+*/
+
+let newArticle = document.createElement('article');
+let newHeading = document.createElement('h3');
+let newParagraph = document.createElement('p');
+
+newHeading.textContent = 'Article 004';
+newParagraph.textContent = 'Hi Guys, I have the opportunity to do some freelance jobs in WordPress and I like to test first in pantheon as we learnt it in the first project';
+
+newArticle.appendChild(newHeading);
+newArticle.appendChild(newParagraph);
+newArticle.classList.add('m-2', 'p-2', 'border', 'border-secondary');
+newArticle.setAttribute('id', 'art-004');
+
+const mainContent = document.querySelector('main');
+
+mainContent.appendChild(newArticle);
+
+
+sidebar.classList.remove('bg-info');
+sidebar.classList.add('bg-warning');
